06b55c075c649a1acf77eb5f3e86f62870e3c1ed
Anna <a.smagina1995@gmail.com>
2023-06-08 02:43:06 +0200

    changed model to yolo

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from transformers import pipeline
+import yolov5
 import torch
+from PIL import Image
+import base64
+import io
+import numpy as np
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -7,20 +11,30 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = yolov5.load('fcakyon/yolov5s-v7.0')
+
+    model.conf = 0.25  # NMS confidence threshold
+    model.iou = 0.45  # NMS IoU threshold
+    model.agnostic = False  # NMS class-agnostic
+    model.multi_label = False  # NMS multiple labels per box
+    model.max_det = 1000  # maximum number of detections per image
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
+   # get the base64 encoded string
+    im_b64 = model_inputs.json['image']
 
-    # Return the results as a dictionary
-    return result
+    # convert it into bytes  
+    img_bytes = base64.b64decode(im_b64.encode('utf-8'))
+
+    # convert bytes data to PIL Image object
+    img = Image.open(io.BytesIO(img_bytes))
+
+    img = np.array(img)
+
+    answer = model(img)
+
+    return str(answer.__len__())
