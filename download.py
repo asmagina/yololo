65b3b3dbd825761f386bf0011506f5cf46bc541e
Anna <a.smagina1995@gmail.com>
2023-06-08 20:12:09 +0200

    fix again

--- download.py
+++ download.py
@@ -3,16 +3,17 @@
 
 # In this example: A Huggingface BERT model
 
-import yolov5
+
+from transformers import pipeline
+from transformers import AutoImageProcessor, AutoModelForObjectDetection
+import torch
 
 def download_model():
-    model = yolov5.load('fcakyon/yolov5s-v7.0')
+    device = 0 if torch.cuda.is_available() else -1
+    image_processor = AutoImageProcessor.from_pretrained("hustvl/yolos-tiny")
+    model = AutoModelForObjectDetection.from_pretrained("hustvl/yolos-tiny")
+    model = pipeline("object-detection", model=model, image_processor=image_processor, device=device)
 
-    model.conf = 0.25  # NMS confidence threshold
-    model.iou = 0.45  # NMS IoU threshold
-    model.agnostic = False  # NMS class-agnostic
-    model.multi_label = False  # NMS multiple labels per box
-    model.max_det = 1000  # maximum number of detections per image
 
 if __name__ == "__main__":
     download_model()
